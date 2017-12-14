1b03f7cf14ba00bc08efb2d36da86d953dd13c35
mairene <iring.ma@gmail.com>
2017-12-13 16:09:45 -0800

    added constructor to define state upon instantiation and defined state.term to be input value

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,17 +4,26 @@ import React, { Component } from 'react';
 
 // define new class SearchBar and has all functionality that React.Component class has
 class SearchBar extends Component {
+  // this function gets called every time instantiated
+  constructor(props) {
+    // Component already has constructor, and parent constructor is called w 'super'
+    super(props);
+
+    // define state as new object w term property
+    this.state = { term: '' };
+  }
   // class must have render method that returns JSX
   render() {
     // javascript variables are put in {}; pass event handler onInputChange to the element that we want to watch for the event
     // onChange is a react property
-    return <input onChange={this.onInputChange} />;
+    // never set state like this.state.term = 'hi' -- use setState; but state can be referenced to get current state like this.state.term
+    return (
+      <div>
+        <input onChange={(event) => this.setState({ term: event.target.value })} />
+      </div>
+    );
   }
 
-  // event handler
-  onInputChange(event){
-    console.log(event);
-  }
 }
 
 // makes this class available to all other files that import SearchBar
